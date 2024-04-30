ef62e22354f5a0fb106f8f0c09bd84015d283040
Nandini Chatterjee <127988967+py-dev-nandini-12@users.noreply.github.com>
2024-04-30 16:02:11 +0100

    Update s3_utils.py

--- Utils/s3_utils.py
+++ Utils/s3_utils.py
@@ -6,9 +6,9 @@ def upload_to_s3(bucket_name, file_name, file_data):
     try:
         s3 = boto3.client('s3',
                           aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                          aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
-        s3.upload_fileobj(file_data, bucket_name, file_name)
-        return f"s3://{bucket_name}/{file_name}"
+                          aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),region_name=os.environ.get('S3_REGION'))
+        s3.upload_fileobj(file_data, bucket_name, file_name,ExtraArgs={'ContentType': 'image/jpeg'})
+        return f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
     except NoCredentialsError:
         raise Exception("S3 credentials not available")
     except Exception as e:
