a77244609d8a5fdbf3e7f95162e21801f8cae79a
Nandini Chatterjee <127988967+py-dev-nandini-12@users.noreply.github.com>
2024-05-30 16:01:33 +0100

    Update s3_utils.py

--- Utils/s3_utils.py
+++ Utils/s3_utils.py
@@ -2,21 +2,47 @@ import os
 import boto3
 from botocore.exceptions import NoCredentialsError
 
-S3_BUCKET=os.environ.get('S3_BUCKET')
-S3_REGION=os.environ.get('S3_REGION')
+# S3_BUCKET=os.environ.get('S3_BUCKET')
+# S3_REGION=os.environ.get('S3_REGION')
+
+# def upload_to_s3(bucket_name, file_name, file_data):
+#     try:
+#         # Initialize S3 client
+#         s3 = boto3.client('s3',
+#                           aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+#                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+#                           region_name=os.getenv('AWS_REGION'))
+#         s3.upload_fileobj(file_data, bucket_name, file_name,ExtraArgs={'ContentType': 'image/jpeg'})
+#         return f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
+#     except NoCredentialsError:
+#         raise Exception("S3 credentials not available")
+#     except Exception as e:
+#         raise Exception(f"Exception in upload_to_s3: {str(e)}")
+
 
 def upload_to_s3(bucket_name, file_name, file_data):
     try:
+        # Print environment variables
+        # print("S3_BUCKET:", os.getenv('S3_BUCKET_NAME'))
+        # print("S3_REGION:", os.getenv('AWS_REGION'))
+        # print("AWS_ACCESS_KEY_ID:", os.getenv('AWS_ACCESS_KEY_ID'))
+        # print("AWS_SECRET_ACCESS_KEY:", os.getenv('AWS_SECRET_ACCESS_KEY'))
+
         # Initialize S3 client
         s3 = boto3.client('s3',
                           aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
                           region_name=os.getenv('AWS_REGION'))
-        s3.upload_fileobj(file_data, bucket_name, file_name,ExtraArgs={'ContentType': 'image/jpeg'})
-        return f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
+        
+        # Upload file to S3 bucket
+        s3.upload_fileobj(file_data, bucket_name, file_name, ExtraArgs={'ContentType': 'image/jpeg'})
+        
+        # Return the URL of the uploaded file
+        return f"https://{bucket_name}.s3.{os.getenv('AWS_REGION')}.amazonaws.com/{file_name}"
+    
     except NoCredentialsError:
         raise Exception("S3 credentials not available")
+    
     except Exception as e:
         raise Exception(f"Exception in upload_to_s3: {str(e)}")
-
 
